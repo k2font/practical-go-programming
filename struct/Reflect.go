eb291ba5f1a99c5e0eb589b37e01b5ebf6275811
k2font <shoichiro.kouno@gmail.com>
2023-07-01 14:21:36 +0900

    reflectパッケージで取得したオブジェクトの中身を見るコードを追加

--- struct/Reflect.go
+++ struct/Reflect.go
@@ -1,22 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"reflect"
 )
 
 type MapStruct struct {
-	Str    string  `map:"str"`
-	StrPtr *string `map:"str"`
+	Str     string  `map:"str"`
+	StrPtr  *string `map:"str"`
+	Bool    bool    `map:"bool"`
+	BoolPtr *bool   `map:"bool"`
+	Int     int     `map:"int"`
+	IntPtr  *int    `map:"int"`
 }
 
 func Reflect() {
 	src := map[string]string{
-		"str": "string data",
+		"str":  "string data",
+		"bool": "true",
+		"int":  "1",
 	}
 
 	var ms MapStruct
-	Decode(&ms, src)
+	Decode(&ms, src) // srcのmap[string]stringをデコードしてMapStruct型のmsに格納する
 	log.Println(ms)
 }
 
@@ -26,6 +33,13 @@ func Decode(target interface{}, src map[string]string) {
 	decode(e, src)
 }
 
+// structの型が不明なので、reflectを使ってデコードする
+// これぞメタプログラミングみたいなコードになりそう
 func decode(e reflect.Value, src map[string]string) {
-
+	t := e.Type()
+	fmt.Println(t)
+	for i := 0; i < t.NumField(); i++ {
+		f := t.Field(i)
+		fmt.Println(f)
+	}
 }

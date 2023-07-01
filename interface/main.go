c7417126184c32260fbd0e6057bba97af36e5127
k2font <shoichiro.kouno@gmail.com>
2023-07-01 16:38:44 +0900

    interfaceの基礎を学んだ

---
+++ interface/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/gen2brain/beeep"
+)
+
+func main() {
+	// Show()メソッドを持つインスタンスは何でも入れられる変数
+	var warn Warning
+	// インターフェースを満たす構造体を代入できる
+
+	// コンソールに出力
+	warn = &ConsoleWarning{}
+	warn.Show("Hello World to Console")
+
+	// デスクトップにアラートを出力
+	warn = &DesktopWarning{}
+	warn.Show("Hello World to Desktop")
+}
+
+// すべての構造体の共通インターフェース
+type Warning interface {
+	Show(message string)
+}
+
+// コンソールにメッセージを通知する構造体
+type ConsoleWarning struct{}
+
+// コンソールにメッセージを表示するShow関数
+func (c ConsoleWarning) Show(message string) {
+	fmt.Fprintf(os.Stderr, "[%s]: %s\n", os.Args[0], message)
+}
+
+// デスクトップにメッセージを通知する構造体
+type DesktopWarning struct{}
+
+// デスクトップにアラートを表示するShow関数
+func (d DesktopWarning) Show(message string) {
+	// beeepおもろ。あとでコード読む。
+	beeep.Alert(os.Args[0], message, "")
+}
+
+// Slackにメッセージを通知する構造体
+// TODO: 実装は省略
+type SlackWarning struct{}

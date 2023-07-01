d799066fafb3a4608a6d18de3952a1e39072ac60
k2font <shoichiro.kouno@gmail.com>
2023-07-01 14:41:08 +0900

    インスタンスの型に応じて構造体に値をセットする処理の追加

--- struct/Reflect.go
+++ struct/Reflect.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"reflect"
+	"strconv"
 )
 
 type MapStruct struct {
@@ -16,20 +17,24 @@ type MapStruct struct {
 
 func Reflect() {
 	src := map[string]string{
-		"str":  "string data",
+		"str":  "shoichiro",
 		"bool": "true",
 		"int":  "1",
 	}
 
+	// タグを使った構造体へのデータの書き込み処理
 	var ms MapStruct
-	Decode(&ms, src) // srcのmap[string]stringをデコードしてMapStruct型のmsに格納する
-	fmt.Printf("%+x\n", ms)
+	err := Decode(&ms, src) // srcのmap[string]stringをデコードしてMapStruct型のmsに格納する
+	if err != nil {
+		// TODO: エラー処理
+	}
+	log.Println(ms)
 }
 
 func Decode(target interface{}, src map[string]string) error {
 	v := reflect.ValueOf(target)
 	e := v.Elem()
-	decode(e, src)
+	return decode(e, src)
 }
 
 // structの型が不明なので、reflectを使ってデコードする
@@ -46,6 +51,73 @@ func decode(e reflect.Value, src map[string]string) error {
 				return err
 			}
 			continue
+		}
+
+		// 子要素が構造体であれば再帰処理
+		if f.Type.Kind() == reflect.Struct {
+			if err := decode(e.Field(i), src); err != nil {
+				return err
+			}
+			continue
+		}
+
+		// タグがなければフィールド名を使用する
+		key := f.Tag.Get("map")
+		if key == "" {
+			key = f.Name
+		}
+
+		// 元データになければスキップ
+		sv, ok := src[key]
+		if !ok {
+			continue
+		}
+
+		// フィールドの型を取得
+		var k reflect.Kind
+		var isP bool
+		// 対象の型がポインターではない場合
+		if f.Type.Kind() != reflect.Ptr {
+			k = f.Type.Kind()
+		} else { // 対象の型がポインターの場合
+			k = f.Type.Elem().Kind() // 値を取得してkに格納する
+
+			// もしkがポインターのポインターであれば無視する
+			if k == reflect.Ptr {
+				continue
+			}
+
+			// 対象の値はポインターかどうかを管理するフラグを立てる
+			isP = true
+		}
+
+		// 型に応じて値をセットする
+		switch k {
+		case reflect.String:
+			if isP {
+				e.Field(i).Set(reflect.ValueOf(&sv))
+			} else {
+				e.Field(i).SetString(sv)
+			}
+		case reflect.Bool:
+			b, err := strconv.ParseBool(sv)
+			if err == nil {
+				if isP {
+					e.Field(i).Set(reflect.ValueOf(&b))
+				} else {
+					e.Field(i).SetBool(b)
+				}
+			}
+		case reflect.Int:
+			n64, err := strconv.ParseInt(sv, 10, 64)
+			if err == nil {
+				if isP {
+					n := int(n64)
+					e.Field(i).Set(reflect.ValueOf(&n))
+				} else {
+					e.Field(i).SetInt(n64)
+				}
+			}
 		}
 	}
 	return nil
